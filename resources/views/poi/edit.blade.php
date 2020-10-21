12df2f7459f4ae6575013bcda1fd1890353b5953
Mocktar <44744049+mocktarissa@users.noreply.github.com>
2020-10-21 17:51:13 +0300

    ui

--- resources/views/poi/edit.blade.php
+++ resources/views/poi/edit.blade.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <title>Dashboard</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -21,24 +23,27 @@
         </ul>
     </div>
     @endif
-    <form action="{{route('company.pois.update',['company'=>$company,'poi'=>$poi->id])}}" method="post">
-        @csrf
-        @method('PUT')
-        <label for="name">Name of Attraction</label>
-        <input type="text" id="name" name="name" value="{{$poi->name}}">
-        <label for="location">Location</label>
-        <input type="text" id="location" name="location" value="{{$poi->location}}">
-        <label for="description">Description</label>
-        <input type="text" id="description" name='description' value="{{$poi->description}}">
-        <label for="url"> Url </label>
-        <input type="url" name='url' id='url' value="{{$poi->url}}">
-        <select name="category">
+    <div class="container">
 
-            <option value="{{$poi->category->id}}">{{$poi->category->name}}</option>
+        <form action="{{route('company.pois.update',['company'=>$company,'poi'=>$poi->id])}}" method="post">
+            @csrf
+            @method('PUT')
+            <label for="name">Name of Attraction</label>
+            <input type="text" id="name" name="name" value="{{$poi->name}}" class='form-control'>
+            <label for="location">Location</label>
+            <input type="text" id="location" name="location" value="{{$poi->location}}" class='form-control'>
+            <label for="description">Description</label>
+            <input type="text" id="description" name='description' value="{{$poi->description}}" class='form-control'>
+            <label for="url"> Url </label>
+            <input type="url" name='url' id='url' value="{{$poi->url}}" class='form-control'>
+            <select name="category">
 
-        </select>
-        <button type="submit">Save</button>
-    </form>
+                <option value="{{$poi->category->id}}">{{$poi->category->name}}</option>
+
+            </select>
+            <button type="submit">Save</button>
+        </form>
+    </div>
 
 
 
