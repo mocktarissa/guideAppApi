c15d13d6a085130ad9f0d6bbab3cffd413fd495d
Mocktar <44744049+mocktarissa@users.noreply.github.com>
2020-11-17 15:45:08 +0300

    search page works !!!

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -18,25 +18,24 @@ class SearchController extends Controller
 
         $companies = DB::table('companys')->select('*')
             ->whereRaw(
-                'LOWER(`name`) LIKE ? ',
+                'lower(`name`) LIKE ? ',
                 [trim(strtolower($request->input('query')) . '%')],
 
-
             )->orWhereRaw(
-                'LOWER(`city`) LIKE ? ',
+                'lower(`city`) LIKE ? ',
                 [trim(strtolower($request->input('query')) . '%')],
             )->orWhereRaw(
-                'LOWER(`category`) LIKE ? ',
+                'lower(`category`) LIKE ? ',
                 [trim(strtolower($request->input('query')) . '%')],
             )
             ->get();
 
         $pois = DB::table('pois')->leftJoin('categories', 'categories.company_id', '=', 'pois.company_id')->select('pois.*')
             ->whereRaw(
-                'LOWER(`categories.name`) LIKE ? ',
+                'lower(`categories`.`name`) LIKE ? ',
                 [trim(strtolower($request->input('query')) . '%')],
             )->orWhereRaw(
-                'LOWER(`pois.name`) LIKE ? ',
+                'lower(`pois`.`name`) LIKE ? ',
                 [trim(strtolower($request->input('query')) . '%')],
             )->groupBy('pois.id')
             ->get();
